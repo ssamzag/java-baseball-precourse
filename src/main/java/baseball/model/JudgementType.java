08ac2d7855b89599bb08d6568fda5814ff2d278a
Choe minho <>
2022-04-18 13:00:59 +0900

    refactor: 스트라이크/볼/낫씽 상수문자열을 enum toString 처리

--- src/main/java/baseball/model/JudgementType.java
+++ src/main/java/baseball/model/JudgementType.java
@@ -1,7 +1,18 @@
 package baseball.model;
 
 public enum JudgementType {
-    STRIKE,
-    BALL,
-    NOTHING
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTHING("낫싱");
+
+    private final String name;
+    JudgementType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
 }
--- src/main/java/baseball/ui/ResultView.java
+++ src/main/java/baseball/ui/ResultView.java
@@ -1,14 +1,13 @@
 package baseball.ui;
 
 import baseball.model.BaseballRecord;
+import baseball.model.JudgementType;
+import baseball.model.PlayStatus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ResultView {
-    private static final String OUT_MESSAGE = "낫싱";
-    private static final String BALL_MESSAGE = "볼";
-    private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String GAME_EXIT = "게임을 종료합니다.";
     private static final String DELIMITER = " ";
@@ -19,15 +18,15 @@ public class ResultView {
 
     private static String getJudgementString(BaseballRecord baseballRecord) {
         if (baseballRecord.hasOut()) {
-            return OUT_MESSAGE;
+            return JudgementType.NOTHING.toString();
         }
 
         List<String> judgementList = new ArrayList<>();
         if (baseballRecord.hasBall()) {
-             judgementList.add(baseballRecord.getBallCount() + BALL_MESSAGE);
+             judgementList.add(baseballRecord.getBallCount() + JudgementType.BALL.toString());
         }
         if (baseballRecord.hasStrike()) {
-            judgementList.add(baseballRecord.getStrikeCount() + STRIKE_MESSAGE);
+            judgementList.add(baseballRecord.getStrikeCount() + JudgementType.STRIKE.toString());
         }
 
         return String.join(DELIMITER, judgementList);
